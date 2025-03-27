03fd9747d9340059e343be4cc8f1c6a833486156
Hannes Piechulek <hanpie0501@gmail.com>
2025-03-27 11:42:30 +0100

    added selenium stealth and reusing session

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+from selenium_stealth import stealth
+
 # Set Chrome options
-chrome_options = Options()
-#chrome_options.add_argument("--headless=new")  # Run in headless mode (optional)
-chrome_options.add_argument("--disable-gpu")
+options = Options()
+#options.add_argument("--headless=new")  # Run in headless mode (optional)
+options.add_argument("user-data-dir=/home/hannes-piechulek/.config/google-chrome")
 
 # Initialize ChromeDriver
 service = Service(ChromeDriverManager().install())  # Auto-downloads the driver
-driver = webdriver.Chrome(service=service, options=chrome_options)
+driver = webdriver.Chrome(service=service, options=options)
 
-# Open a website
-driver.get("https://www.google.com")
-print("Page title:", driver.title)
+# set stealth options
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Linux x86_64",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
 
-# Close the browser
+# Open website
+driver.get("https://www.instagram.com")
+
+sleep(30)
 driver.quit()
