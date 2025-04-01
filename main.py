054329d250c04b690c5c15c82f659d1bc73322e6
Hannes Piechulek <hanpie0501@gmail.com>
2025-04-01 21:22:59 +0200

    turned script into function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from time import sleep
+import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -7,20 +8,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 
 from humancursor import WebCursor
-# from https://github.com/saadejazz/humanTyper
 from packages.typer import Typer
 from selenium_stealth import stealth
 
+
 # Set Chrome options
 options = Options()
-#options.add_argument("--headless=new")  # Run in headless mode (optional)
 options.add_argument("user-data-dir=/home/hannes-piechulek/.config/google-chrome")
 
 # Initialize ChromeDriver
-service = Service(ChromeDriverManager().install())  # Auto-downloads the driver
+service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
 # set stealth options
@@ -33,120 +33,90 @@ stealth(driver,
         fix_hairline=True,
         )
 
-# Open website
-driver.get("https://www.instagram.com")
-
+# Define human cursor and typer
 cursor = WebCursor(driver)
 ty = Typer(accuracy = 0.90, correction_chance = 0.70, typing_delay = (0.06, 0.09), distance = 2)
 
+# Open website
+driver.get("https://www.instagram.com")
 
-##################SEARCH########################
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Search')]"))
-)
+def search_profile(username: str):
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Search')]")))
+    search_element = driver.find_element(By.XPATH, "//span[contains(text(), 'Search')]")
+    cursor.click_on(search_element)
 
-search_element = driver.find_element(By.XPATH, "//span[contains(text(), 'Search')]")
-x = search_element.location["x"]
-y = search_element.location["y"]
+    WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, "//input[@aria-label='Search input']")))
+    search_input_element = driver.find_element(By.XPATH, "//input[@aria-label='Search input']")
+    ty.send(search_input_element, f"@{username}")
+    sleep(random.uniform(0.5, 1))
 
-cursor.click_on(search_element)
-
-
-WebDriverWait(driver, 2).until(
-    EC.presence_of_element_located((By.XPATH, "//input[@aria-label='Search input']"))
-)
-search_input_element = driver.find_element(By.XPATH, "//input[@aria-label='Search input']")
-ty.send(search_input_element, "@hansampie")
-sleep(1)
-
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'hansampie')]"))
-)
-profile_element = driver.find_element(By.XPATH, "//span[contains(text(), 'hansampie')]")
-cursor.click_on(profile_element)
-sleep(1)
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'hansampie')]")))
+    profile_element = driver.find_element(By.XPATH, "//span[contains(text(), 'hansampie')]")
+    cursor.click_on(profile_element)
+    sleep(random.uniform(0.5, 1))
 
 
-##################FOLLOW COUNTS########################
-# TODO Test
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.XPATH, '//ul/li[2]/div/a/span/span/span'))
-)
-followers_count = int(driver.find_element(By.XPATH, '//ul/li[2]/div/a/span/span/span').text)
+def get_follow_count() -> tuple[int, int]:
+    """
+    returns first the followers count and second the following count
+    """
 
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.XPATH, '//ul/li[3]/div/a/span/span/span'))
-)
-following_count = int(driver.find_element(By.XPATH, '//ul/li[3]/div/a/span/span/span').text)
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//ul/li[2]/div/a/span/span/span')))
+    followers_count = driver.find_element(By.XPATH, '//ul/li[2]/div/a/span/span/span').text
 
-if followers_count > 2000 or following_count > 2000:
-    pass
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//ul/li[3]/div/a/span/span/span')))
+    following_count = driver.find_element(By.XPATH, '//ul/li[3]/div/a/span/span/span').text
 
+    return int(followers_count), int(following_count)
 
-##################FOLLOWERS########################
-# TODO Test
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'followers')]"))
-)
-followers_element = driver.find_element(By.XPATH, "//span[contains(text(), 'followers')]")
-cursor.click_on(followers_element)
-sleep(1)
+def get_usernames(followers=False, following=False) -> list[str]:
+    path = "followers" if followers == True else "following"
 
-try:
-    while True:
-        scrollbar = driver.find_element(By.XPATH, "//div[@data-visualcompletion='loading-state']")
-        cursor.scroll_into_view_of_element(scrollbar)
-        sleep(0.2)
-except NoSuchElementException:
-    pass
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, f"//span[contains(text(), '{path}')]")))
+    followers_element = driver.find_element(By.XPATH, f"//span[contains(text(), '{path}')]")
+    cursor.click_on(followers_element)
+    sleep(random.uniform(0.5, 1))
 
-dialog = driver.find_element(By.XPATH, "//div[@role='dialog']")
-# username_elements = dialog.find_elements(By.XPATH, ".//span[string-length(text()) > 0]")
-# username_elements = text[1:]
-username_elements = dialog.find_elements(By.XPATH, '//span[contains(@class, "_ap3a") and contains(@class, "_aaco") and contains(@class, "_aacw") and contains(@class, "_aacx") and contains(@class, "_aad7") and contains(@class, "_aade")]')
-followers = [ element.text for element in username_elements]
+    try:
+        while True:
+            WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, "//div[@data-visualcompletion='loading-state']")))
+            scrollbar = driver.find_element(By.XPATH, "//div[@data-visualcompletion='loading-state']")
+            cursor.scroll_into_view_of_element(scrollbar)
+            sleep(random.uniform(0.1, 0.3))
+    except TimeoutException:
+        pass
 
-close_button_element = dialog.find_element(By.XPATH, "//button[contains(@class, '_abl-')]")
-cursor.click_on(close_button_element)
+    dialog = driver.find_element(By.XPATH, "//div[@role='dialog']")
+    username_elements = dialog.find_elements(By.XPATH, '//span[contains(@class, "_ap3a") and contains(@class, "_aaco") and contains(@class, "_aacw") and contains(@class, "_aacx") and contains(@class, "_aad7") and contains(@class, "_aade")]')
+    usernames = [ element.text for element in username_elements]
 
-##################FOLLOWING#######################
-# TODO Test
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'following')]"))
-)
-following_element = driver.find_element(By.XPATH, "//span[contains(text(), 'following')]")
-cursor.click_on(following_element)
-sleep(1)
+    close_button_element = dialog.find_element(By.XPATH, "//button[contains(@class, '_abl-')]")
+    cursor.click_on(close_button_element)
+    sleep(random.uniform(0.5, 1))
 
-try:
-    while True:
-        scrollbar = driver.find_element(By.XPATH, "//div[@data-visualcompletion='loading-state']")
-        cursor.scroll_into_view_of_element(scrollbar)
-        sleep(0.2)
-except NoSuchElementException:
-    pass
+    return usernames
 
-dialog = driver.find_element(By.XPATH, "//div[@role='dialog']")
-# username_elements = dialog.find_elements(By.XPATH, ".//span[string-length(text()) > 0]")
-# username_elements = text[1:]
-username_elements = dialog.find_elements(By.XPATH, '//span[contains(@class, "_ap3a") and contains(@class, "_aaco") and contains(@class, "_aacw") and contains(@class, "_aacx") and contains(@class, "_aad7") and contains(@class, "_aade")]')
-following = [ element.text for element in username_elements]
+def scrape_profile(username: str) -> dict:
+    profile = {
+        'username': username
+    }
+    search_profile(username)
+    profile['followers_count'], profile['following_count'] = get_follow_count()
+    if profile['followers_count'] > 2000 or profile['following_count'] > 2000:
+        return profile
 
-close_button_element = dialog.find_element(By.XPATH, "//button[contains(@class, '_abl-')]")
-cursor.click_on(close_button_element)
+    profile['followers'] = get_usernames(followers=True)
+    profile['following'] = get_usernames(following=True)
 
-##################DEBUG#######################
-# for u in followers: 
-#     print(u)
-print("Followers: " + str(len(followers)))
-print(followers_count)
-if len (followers) != followers_count: print("fuck")
-print("---------------------------------------------------")
-# for u in following: 
-#     print(u)
-print("Following: " + str(len(following)))
-print(following_count)
-if len (following) != following_count: print("fuck")
+    followers_not_equal = len(profile['followers']) != profile['followers_count']
+    following_not_equal =  len(profile['following']) != profile['following_count']
 
-sleep(30)
+    if followers_not_equal or following_not_equal:
+        print(profile)
+        raise ValueError("Follow count mismatch!")
+    
+    return profile
+    
+print(scrape_profile("hansampie"))
+
 driver.quit()
